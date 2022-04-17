c21a3b570e50d4725dbb8f2627f4e6209284f41b
Shair Dilavar <59974771+teetotu@users.noreply.github.com>
2022-04-17 14:46:55 +0300

    revert rm expiresAr

--- src/main/java/ru/hse/forum/dto/AuthenticationResponse.java
+++ src/main/java/ru/hse/forum/dto/AuthenticationResponse.java
@@ -14,5 +14,6 @@ import java.time.Instant;
 public class AuthenticationResponse {
     private String authenticationToken;
     private String refreshToken;
+    private Instant expiresAt;
     private String username;
 }
--- src/main/java/ru/hse/forum/service/AuthService.java
+++ src/main/java/ru/hse/forum/service/AuthService.java
@@ -111,6 +111,7 @@ public class AuthService {
         return AuthenticationResponse.builder()
                 .authenticationToken(token)
                 .refreshToken(refreshTokenService.generateRefreshToken().getToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
                 .username(loginRequest.getUsername())
                 .build();
     }
@@ -121,6 +122,7 @@ public class AuthService {
         return AuthenticationResponse.builder()
                 .authenticationToken(token)
                 .refreshToken(refreshTokenRequest.getRefreshToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
                 .username(refreshTokenRequest.getUsername())
                 .build();
     }
