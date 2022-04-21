f464e35c024a9882722e699d3cc3b111ecbf79b4
Shair Dilavar <59974771+teetotu@users.noreply.github.com>
2022-04-21 20:55:44 +0300

    fix fts

--- src/main/java/ru/hse/forum/util/SqlFunctionsMetadataBuilderContributor.java
+++ src/main/java/ru/hse/forum/util/SqlFunctionsMetadataBuilderContributor.java
@@ -8,10 +8,10 @@ import org.hibernate.type.BooleanType;
 public class SqlFunctionsMetadataBuilderContributor implements MetadataBuilderContributor {
     @Override
     public void contribute(MetadataBuilder metadataBuilder) {
-        metadataBuilder.applySqlFunction("ftss",
+        metadataBuilder.applySqlFunction("ftsp",
                 new SQLFunctionTemplate(BooleanType.INSTANCE,
                         "to_tsvector(post_title) @@ plainto_tsquery(?1)"));
-        metadataBuilder.applySqlFunction("ftsp",
+        metadataBuilder.applySqlFunction("ftss",
                 new SQLFunctionTemplate(BooleanType.INSTANCE,
                         "to_tsvector(name) @@ plainto_tsquery(?1)"));
     }
